6746925b5dd2de9857474199fcbd50cd89c5a802
NeuroGirl <vesus0206@gmail.com>
2024-09-14 22:00:55 +0300

    Update 1.4.py

--- lab 0 aisd/1.4.py
+++ lab 0 aisd/1.4.py
@@ -1,12 +1,9 @@
-import time
-t_start = time.perf_counter()
 f = open("input.txt")
 nums = f.readline().split(" ")
 a = int(nums[0])
 b = int(nums[1])
-if -10**9 < a < 10**9 and -10**9 < a < 10**9:
+if -10**9 <= a <= 10**9 and -10**9 <= b <= 10**9:
     new_f = open("output.txt", "w+")
     new_f.write(f'{a + b**2}')
     new_f.close()
 f.close()
-print(time.perf_counter() - t_start)
