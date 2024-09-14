2174d0a8d37089311236b7db677a0db104d3cf4d
NeuroGirl <vesus0206@gmail.com>
2024-09-14 22:01:07 +0300

    Update 1.3.py

--- lab 0 aisd/1.3.py
+++ lab 0 aisd/1.3.py
@@ -2,7 +2,7 @@ f = open("input.txt")
 nums = f.readline().split(" ")
 a = int(nums[0])
 b = int(nums[1])
-if -10**9 <= a <= 10**9 and -10**9 <= a <= 10**9:
+if -10**9 <= a <= 10**9 and -10**9 <= b <= 10**9:
     new_f = open("output.txt", "w+")
     new_f.write(f'{a + b}')
     new_f.close()
