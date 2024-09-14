0d89cf5e50838f3bfd009b847532177450c5a7ab
NeuroGirl <vesus0206@gmail.com>
2024-09-14 21:10:12 +0300

    Update 1.1.py

--- lab 0 aisd/1.1.py
+++ lab 0 aisd/1.1.py
@@ -1,8 +1,5 @@
-import time
-t_start = time.perf_counter()
 nums = input().split(" ")
 a = int(nums[0])
 b = int(nums[1])
-if -10**9 < a < 10**9 and -10**9 < a < 10**9:
+if -10**9 <= a <= 10**9 and -10**9 <= b <= 10**9:
     print(a + b)
-print(time.perf_counter() - t_start)
