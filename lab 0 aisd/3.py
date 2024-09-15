13f569b2dde12b17c6a7d92f74e337fe9df2a7c8
NeuroGirl <vesus0206@gmail.com>
2024-09-15 21:26:52 +0300

    Update 3.py

--- lab 0 aisd/3.py
+++ lab 0 aisd/3.py
@@ -3,10 +3,11 @@ t_start = time.perf_counter()
 d = open("input.txt")
 n = int(d.readline())
 a = [0, 1]
-for i in range(n-1):
-    a.append(a[-1] + a[-2])
 if 0 <= n <= 10**7:
+    for i in range(n):
+    	a.append(a[-1] + a[-2])
     new_f = open("output.txt", "w+")
-    new_f.write(f'{a[-1] % 10}')
+    new_f.write(f'{a[-2]  % 10}')
     new_f.close()
+d.close()
 print(time.perf_counter() - t_start)
