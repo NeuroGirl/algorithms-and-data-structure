a67f1571c41bdf59b1b288caacba65bc7326af1d
NeuroGirl <vesus0206@gmail.com>
2024-09-16 18:38:09 +0300

    Add files via upload

---
+++ Лаба Аисд/1.1.py
@@ -0,0 +1,12 @@
+nums = input().split(" ")
+a = int(nums[0])
+b = int(nums[1])
+
+while (not(-10**9 <= a <= 10**9)) or (not(-10**9 <= b <= 10**9)):
+    print("Неверные входные данные")
+    nums = input().split(" ")
+    a = int(nums[0])
+    b = int(nums[1])
+
+if -10**9 <= a <= 10**9 and -10**9 <= b <= 10**9:
+    print(a + b)
