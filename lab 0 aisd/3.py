ced6657de35198dc7796ea0faa75f98838f9fb2c
NeuroGirl <vesus0206@gmail.com>
2024-09-16 17:46:29 +0300

    Update 3.py

--- lab 0 aisd/3.py
+++ lab 0 aisd/3.py
@@ -1,17 +1,17 @@
 d = open("input.txt")
 n = int(d.readline())
-f1 = 0
-f2 = 1
-temp = 0
-if 0 < n <= 10**7:
-    for i in range(n-1):
-        temp = f1 % 10
-        f1 = f2 % 10
-        f2 = (f1 + temp) % 10
+a = [0, 1]
+
+while not(0 <= n <= 45):
+    print("Неверные входные данные")
+    time.sleep(4)
+    d = open("input.txt")
+    n = int(d.readline())
+
+if 0 <= n <= 45:
+    for i in range(n):
+        a.append(a[-1] + a[-2])
     new_f = open("output.txt", "w+")
-    new_f.write(f'{f2}')
-elif n == 0:
-    new_f = open("output.txt", "w+")
-    new_f.write(f'{f1}')
-new_f.close()
+    new_f.write(f'{a[-2]}')
+    new_f.close()
 d.close()
