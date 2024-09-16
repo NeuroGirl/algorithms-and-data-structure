a3c07009a731e074d1b4bf5d07be0588eb001854
NeuroGirl <vesus0206@gmail.com>
2024-09-16 17:46:54 +0300

    Update 2.py

--- lab 0 aisd/2.py
+++ lab 0 aisd/2.py
@@ -1,9 +1,16 @@
 d = open("input.txt")
 n = int(d.readline())
 a = [0, 1]
+
+while not(0 <= n <= 45):
+    print("Неверные входные данные")
+    time.sleep(4)
+    d = open("input.txt")
+    n = int(d.readline())
+
 if 0 <= n <= 45:
     for i in range(n):
-    	a.append(a[-1] + a[-2])
+        a.append(a[-1] + a[-2])
     new_f = open("output.txt", "w+")
     new_f.write(f'{a[-2]}')
     new_f.close()
