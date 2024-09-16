f083624abc62671d1a99466e826214deb1e1f0c8
NeuroGirl <vesus0206@gmail.com>
2024-09-16 17:48:36 +0300

    Update 1.2.py

--- lab 0 aisd/1.2.py
+++ lab 0 aisd/1.2.py
@@ -3,6 +3,7 @@ a = int(nums[0])
 b = int(nums[1])
 
 while (not(-10**9 <= a <= 10**9)) or (not(-10**9 <= b <= 10**9)):
+    print("Неверные входные данные")
     nums = input().split(" ")
     a = int(nums[0])
     b = int(nums[1])
