822fe84e40a458698c55c4bfe45d6f80b1623333
NeuroGirl <vesus0206@gmail.com>
2024-09-16 17:44:08 +0300

    Update 1.1.py

--- lab 0 aisd/1.1.py
+++ lab 0 aisd/1.1.py
@@ -1,5 +1,11 @@
 nums = input().split(" ")
 a = int(nums[0])
 b = int(nums[1])
+
+while (not(-10**9 <= a <= 10**9)) or (not(-10**9 <= b <= 10**9)):
+    nums = input().split(" ")
+    a = int(nums[0])
+    b = int(nums[1])
+
 if -10**9 <= a <= 10**9 and -10**9 <= b <= 10**9:
     print(a + b)
