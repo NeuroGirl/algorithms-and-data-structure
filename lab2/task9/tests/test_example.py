35f9f99f191a879636f1c629f7de866259062043
NeuroGirl <vesus0206@gmail.com>
2024-10-20 20:10:36 +0300

    Update test_example.py

--- lab2/task9/tests/test_example.py
+++ lab2/task9/tests/test_example.py
@@ -15,7 +15,7 @@ start_time = time.perf_counter()
 tracemalloc.start()
 list_a = '1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16'
 list_b = '9 8 7 6 5 4 3 2 1 16 15 14 13 12 11 10'
-result = strassen_apply(2, list(map(int, list(list_a.split(' ')))), list(map(int, list(list_b.split(' ')))))
+result = strassen_apply(4, list(map(int, list(list_a.split(' ')))), list(map(int, list(list_b.split(' ')))))
 print(result)
 print('Время работы: ' + str((time.perf_counter() - start_time)))
 print('Память: ' + str(tracemalloc.get_traced_memory()[1]/1024**2) + ' Мб')
