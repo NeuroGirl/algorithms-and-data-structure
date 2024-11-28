7a96ea58bbf14d245249914f6c45c04c0b80e600
NeuroGirl <vesus0206@gmail.com>
2024-11-28 13:57:29 +0300

    Update test_digital_sort.py

--- algorithms-and-data-structures/lab3/task7/tests/test_digital_sort.py
+++ algorithms-and-data-structures/lab3/task7/tests/test_digital_sort.py
@@ -8,6 +8,7 @@ sys.path.insert(0, src_dir)
 import unittest
 from task7.src.digital_sort import radix_sort_phase
 
+
 class TestRadixSort(unittest.TestCase):
 
     def test_should_make_radix_sort_phase(self):
@@ -17,14 +18,15 @@ class TestRadixSort(unittest.TestCase):
             (2, "bba"),
             (3, "baa")
         ]
+
         # when
-        sorted_strings_phase_2 = radix_sort_phase(strings, 1)
+        sorted_strings_phase_2 = radix_sort_phase(strings, 3)
 
         # then
         self.assertEqual(sorted_strings_phase_2, [
-            (1, "bab"),
-            (3, "baa"),
-            (2, "bba")
+            (2, 'bba'),
+            (3, 'baa'),
+            (1, 'bab')
         ])
 
 if __name__ == '__main__':
