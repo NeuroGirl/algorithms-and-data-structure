9e700534049320c0d6a909eba02be4c81f4d6ae7
NeuroGirl <vesus0206@gmail.com>
2024-12-23 19:25:50 +0300

    Исправление названия переменных

--- lab3/task1/src/random_quick_sort_improved.py
+++ lab3/task1/src/random_quick_sort_improved.py
@@ -9,32 +9,32 @@ import random
 from utils import read_from_file, write_in_file, measuring
 
 
-def partition3(A, l, r):
-    x = A[l]
+def partition3(arr, l, r):
+    x = arr[l]
     m1 = l
     m2 = r
     i = l
     while i <= m2:
-        if A[i] < x:
-            A[m1], A[i] = A[i], A[m1]
+        if arr[i] < x:
+            arr[m1], arr[i] = arr[i], arr[m1]
             m1 += 1
             i += 1
-        elif A[i] > x:
-            A[i], A[m2] = A[m2], A[i]
+        elif arr[i] > x:
+            arr[i], arr[m2] = arr[m2], arr[i]
             m2 -= 1
         else:
             i += 1
     return m1, m2
 
 
-def randomized_quick_sort_p3(A, l, r):
+def randomized_quick_sort_p3(arr, l, r):
     if l < r:
         k = random.randint(l, r)
-        A[l], A[k] = A[k], A[l]
-        m1, m2 = partition3(A, l, r)
-        randomized_quick_sort_p3(A, l, m1-1)
-        randomized_quick_sort_p3(A, m2+1, r)
-    return A
+        arr[l], arr[k] = arr[k], arr[l]
+        m1, m2 = partition3(arr, l, r)
+        randomized_quick_sort_p3(arr, l, m1-1)
+        randomized_quick_sort_p3(arr, m2+1, r)
+    return arr
 
 
 if __name__ == '__main__':
