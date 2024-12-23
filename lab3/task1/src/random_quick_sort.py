ec5355a26a77704193d472f4a1552956edb2f0ae
NeuroGirl <vesus0206@gmail.com>
2024-12-23 17:18:24 +0300

    Исправление ошибок, обнаруженных Sonar-ом 5

--- lab3/task1/src/random_quick_sort.py
+++ lab3/task1/src/random_quick_sort.py
@@ -9,25 +9,25 @@ import random
 from utils import read_from_file, write_in_file, measuring
 
 
-def partition(A, l, r):
-    x = A[l]
+def partition(arr, l, r):
+    x = arr[l]
     j = l
     for i in range(l+1, r+1):
-        if A[i] < x:
+        if arr[i] < x:
             j += 1
-            A[j], A[i] = A[i], A[j]
-    A[l], A[j] = A[j], A[l]
+            arr[j], arr[i] = arr[i], arr[j]
+    arr[l], arr[j] = arr[j], arr[l]
     return j
 
 
-def randomized_quick_sort(A, l, r):
+def randomized_quick_sort(arr, l, r):
     if l < r:
         k = random.randint(l, r)
-        A[l], A[k] = A[k], A[l]
-        m = partition(A, l, r)
-        randomized_quick_sort(A, l, m-1)
-        randomized_quick_sort(A, m+1, r)
-    return A
+        arr[l], arr[k] = arr[k], arr[l]
+        m = partition(arr, l, r)
+        randomized_quick_sort(arr, l, m-1)
+        randomized_quick_sort(arr, m+1, r)
+    return arr
 
 
 if __name__ == '__main__':
