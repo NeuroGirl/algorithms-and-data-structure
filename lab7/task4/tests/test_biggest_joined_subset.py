50024f876f18250f1695ed7a88502763112d2f8a
NeuroGirl <vesus0206@gmail.com>
2024-12-23 16:52:11 +0300

    Исправление ошибок, обнаруженных Sonar-ом 2

--- lab7/task4/tests/test_biggest_joined_subset.py
+++ lab7/task4/tests/test_biggest_joined_subset.py
@@ -13,9 +13,7 @@ class TestBiggestJoinedSubset(unittest.TestCase):
 
     def test_should_check_the_possibility_of_calculating_the_length_of_two_1st_sets_joined_subset(self):
         # given
-        n = 4
         array_1 = ['2', '7', '8', '3']
-        m = 4
         array_2 = ['5', '2', '8', '7']
         expected_result = 2
         
@@ -27,9 +25,7 @@ class TestBiggestJoinedSubset(unittest.TestCase):
 
     def test_should_check_the_possibility_of_calculating_the_length_of_two_2nd_sets_joined_subset(self):
         # given
-        n = 1
         array_1 = ["7"]
-        m = 4
         array_2 = ["1", "2", "3", "4"]
         expected_result = 0
 
--- lab7/task5/tests/test_biggest_joined_subset_3.py
+++ lab7/task5/tests/test_biggest_joined_subset_3.py
@@ -13,11 +13,8 @@ class TestBiggestJoinedSubset3(unittest.TestCase):
 
     def test_should_check_the_possibility_of_calculating_the_length_of_three_1st_sets_joined_subset(self):
         # given
-        n = 3
         array_1 = ['1', '2', '3']
-        m = 3
         array_2 = ['2', '1', '3']
-        k = 3
         array_3 = ['1', '3', '5']
         expected_result = 2
 
@@ -29,11 +26,8 @@ class TestBiggestJoinedSubset3(unittest.TestCase):
 
     def test_should_check_the_possibility_of_calculating_the_length_of_three_2nd_sets_joined_subset(self):
         # given
-        n = 5
         array_1 = ['8', '3', '2', '1', '7']
-        m = 7
         array_2 = ['8', '2', '1', '3', '8', '10', '7']
-        k = 6
         array_3 = ['6', '8', '3', '1', '4', '7']
         expected_result = 3
 
